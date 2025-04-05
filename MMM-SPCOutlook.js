6fc5ef3fa7d9409effc37af358995759296c9e37
Kyle Creasey <lightning.jim@gmail.com>
2025-04-05 13:43:16 -0500

    Working model, still need to add in SIGN identification and Days 4-8

--- MMM-SPCOutlook.js
+++ MMM-SPCOutlook.js
@@ -9,12 +9,14 @@ Module.register("MMM-SPCOutlook", {
     Log.info(`Starting module: ${this.name}`);
     this.sendSocketNotification("GET_SPC_DATA", { lat: this.config.lat, lon: this.config.lon });
     console.log("SPC Outlook: GET_SPC_DATA - " + this.config.lat + "," + this.config.lon);
+    // Set an interval to update every hour (3600000 milliseconds)
+    setInterval(() => {this.sendSocketNotification("GET_SPC_DATA", { lat: this.config.lat, lon: this.config.lon });}, 3600000);
   },
 
   socketNotificationReceived: function(notification, payload) {
     if (notification === "SPC_DATA_RESULT") {
       // Store the results in a variable for display
-      console.log("SPC Outlook: SPC_DATA_RESULT Received - " + payload)
+      console.log("SPC Outlook: SPC_DATA_RESULT Received - " + JSON.stringify(payload));
       this.spcrisk = payload;
       this.updateDom();
     }
@@ -26,11 +28,30 @@ Module.register("MMM-SPCOutlook", {
       wrapper.innerHTML = "Loading SPC Outlook...";
     } else if (this.spcrisk.error) {
       wrapper.innerHTML = "Error: " + this.spcrisk.error;
+    } else if (this.spcrisk.day1.risk == "NONE" && this.spcrisk.day2.risk == "NONE" && this.spcrisk.day3.risk == "NONE") {
+      wrapper.innerHTML = "No Severe Weather Risk"
     } else {
       // e.g. day1 risk is in spcrisk.day1
       wrapper.innerHTML = "Day 1: <span style=\"color:#" + this.spcrisk.day1.color + "\">" + this.spcrisk.day1.text + "</span><br/>";
-      //wrapper.innerHTML += "Day 2: <span style=\"color:#" + this.spcrisk.day2.color + "\">" + this.spcrisk.day2.text + "</span><br/>";
-      // You can expand to show day2, day3, etc.
+      if(this.spcrisk.day1.probRisk) {
+        probRiskHTML = ""
+        if (this.spcrisk.day1.torRisk > 0) probRiskHTML += "🌪️ " + 100 * this.spcrisk.day1.torRisk + "% ";
+        if (this.spcrisk.day1.hailRisk > 0) probRiskHTML += "⚪ " + 100 * this.spcrisk.day1.hailRisk + "% ";
+        if (this.spcrisk.day1.windRisk > 0) probRiskHTML += "🌬️ " + 100 * this.spcrisk.day1.windRisk + "%";
+        wrapper.innerHTML += probRiskHTML+"<br/>";
+      }
+      wrapper.innerHTML += "Day 2: <span style=\"color:#" + this.spcrisk.day2.color + "\">" + this.spcrisk.day2.text + "</span><br/>";
+      if(this.spcrisk.day2.probRisk) {
+        probRiskHTML = ""
+        if (this.spcrisk.day2.torRisk > 0) probRiskHTML += "🌪️ " + 100 * this.spcrisk.day2.torRisk + "% ";
+        if (this.spcrisk.day2.hailRisk > 0) probRiskHTML += "⚪ " + 100 * this.spcrisk.day2.hailRisk + "% ";
+        if (this.spcrisk.day2.windRisk > 0) probRiskHTML += "🌬️ " + 100 * this.spcrisk.day2.windRisk + "%";
+        wrapper.innerHTML += probRiskHTML+"<br/>";
+      }
+      wrapper.innerHTML += "Day 3: <span style=\"color:#" + this.spcrisk.day3.color + "\">" + this.spcrisk.day3.text + "</span><br/>";
+      if(this.spcrisk.day3.probRisk) {
+        wrapper.innerHTML += "🌪️⚪🌬️" + 100 * this.spcrisk.day3.probRisk + "%";
+      }
     }
     return wrapper;
   }
--- node_helper.js
+++ node_helper.js
@@ -10,6 +10,7 @@ module.exports = NodeHelper.create({
   // Called when the front-end (MMM-SPCOutlook.js) sends a socket notification
   socketNotificationReceived: async function(notification, payload) {
     if (notification === "GET_SPC_DATA") {
+    	console.log("SPC Outlook: GET_SPC_DATA GET")
       const { lat, lon } = payload;
       const result = await this.getSpcOutlook(lat, lon);
       // Send the results back to your front-end module
@@ -20,40 +21,116 @@ module.exports = NodeHelper.create({
 
   async getSpcOutlook(lat, lon) {
     try {
-      console.log("SPC-Outlook: I'M IN")
+      //console.log("SPC-Outlook: I'M IN")
 
       // The Python script has “risk_to_value” and “value_to_risk” logic:
       const riskToValue = {
-        NOFC: -1, NONE: 0:, TSTM: 1, MRGL: 2, SLGT: 3, ENH: 4, MDT: 5, HIGH: 6
+        TSTM: 1, MRGL: 2, SLGT: 3, ENH: 4, MDT: 5, HIGH: 6
       };
       const valueToRisk = {
-        -1: "NOFC", 0: "NONE", 1: "TSTM", 2: "MRGL", 3: "SLGT", 4: "ENH", 5: "MDT", 6: "HIGH"
+        1: "TSTM", 2: "MRGL", 3: "SLGT", 4: "ENH", 5: "MDT", 6: "HIGH"
       };
       const valueToFullRisk = {
-        -1: "None Forecasted", 0: "None", 1: "General Thunderstorms", 2: "Marginal", 3: "Slight", 4: "Enhanced", 5: "Moderate", 6: "High"
+        NONE: "None", TSTM: "General Thunderstorms", MRGL: "Marginal", SLGT: "Slight", ENH: "Enhanced", MDT: "Moderate", HIGH: "High"
       };
 
-      const riskToColor = {
-      	NOFC: "afddf6", NONE: "afddf6", TSTM: "d2ffa6", MRGL: "7ac687", SLGT: "f7f690", ENH: "e9c188", MDT: "eb7e82", HIGH: "ff81f8"
-      }; // https://www.spc.noaa.gov/new/css/SPCmain.css
+      const percValueRisk = {
+        0.02: 0.02, 0.05: 0.05, 0.10: 0.10, 0.15: 0.15, 0.30: 0.30
+      }
 
-      const day1Risk = this.checkDayCat(geojson, lat, lon, riskToValue, valueToRisk);
+      const riskToColor = {
+      	NONE: "afddf6", TSTM: "d2ffa6", MRGL: "7ac687", SLGT: "f7f690", ENH: "e9c188", MDT: "eb7e82", HIGH: "ff81f8"
+      }; // https://www.spc.noaa.gov/new/css/SPCmain.css
       // Then repeat for day2, day3, etc.
 
-      const url1 = "https://www.spc.noaa.gov/products/outlook/day1otlk_cat.lyr.geojson";
-      const response1 = await fetch(url1);
-      const geojson1 = await response1.json();
+      url = "https://www.spc.noaa.gov/products/outlook/day1otlk_cat.lyr.geojson";
+      response = await fetch(url);
+      geojson = await response.json();
       const day1Risk = this.checkDayCat(geojson, lat, lon, riskToValue, valueToRisk);
-      console.log("SPC-Outlook: Day 1 Risk got - " + this.day1Risk);
+      //console.log("SPC-Outlook: Day 1 Risk got - " + this.day1Risk);
 
-      const url2 = "https://www.spc.noaa.gov/products/outlook/day2otlk_cat.lyr.geojson";
-      const response = await fetch(url2);
-      const geojson = await response2.json();
+      day1ProbRisk = false; 
+      //Torn
+      url = "https://www.spc.noaa.gov/products/outlook/day1otlk_torn.lyr.geojson";
+      response = await fetch(url);
+      geojson = await response.json();
+      const day1TorRisk = this.checkDayPerc(geojson, lat, lon);
+      console.log("SPC-Outlook: Day 1 Tor Risk" + day1TorRisk)
+      day1TorSign = false;
+      if(day1TorRisk > 0) day1TorSign = this.checkDaySign(geojson, lat, lon);
+      //Hail
+      url = "https://www.spc.noaa.gov/products/outlook/day1otlk_hail.lyr.geojson";
+      response = await fetch(url);
+      geojson = await response.json();
+      const day1HailRisk = this.checkDayPerc(geojson, lat, lon);
+      day1HailSign = false;
+      if(day1HailRisk > 0) day1HailSign = this.checkDaySign(geojson, lat, lon);
+      console.log("SPC-Outlook: Day 1 Hail Risk" + day1HailRisk)
+      //wind
+      url = "https://www.spc.noaa.gov/products/outlook/day1otlk_wind.lyr.geojson";
+      response = await fetch(url);
+      geojson = await response.json();
+      const day1WindRisk = this.checkDayPerc(geojson, lat, lon);
+      day1WindSign = false;
+      if(day1WindRisk > 0) day1HailSign = this.checkDaySign(geojson, lat, lon);
+      console.log("SPC-Outlook: Day 1 Wind Risk" + day1WindRisk)
+
+      if (day1TorRisk > 0 || day1HailRisk > 0 || day1WindRisk > 0) day1ProbRisk = true;
+      console.log("SPC-Outlook: Day 1 Prob Risk test | " + day1ProbRisk)
+
+      url = "https://www.spc.noaa.gov/products/outlook/day2otlk_cat.lyr.geojson";
+      response = await fetch(url);
+      geojson = await response.json();
       const day2Risk = this.checkDayCat(geojson, lat, lon, riskToValue, valueToRisk);
-      console.log("SPC-Outlook: Day 2 Risk got - " + this.day2Risk);
+      day2ProbRisk = false; 
+      //Torn
+      url = "https://www.spc.noaa.gov/products/outlook/day2otlk_torn.lyr.geojson";
+      response = await fetch(url);
+      geojson = await response.json();
+      const day2TorRisk = this.checkDayPerc(geojson, lat, lon);
+      console.log("SPC-Outlook: Day 2 Tor Risk" + day1TorRisk)
+      //Hail
+      url = "https://www.spc.noaa.gov/products/outlook/day2otlk_hail.lyr.geojson";
+      response = await fetch(url);
+      geojson = await response.json();
+      const day2HailRisk = this.checkDayPerc(geojson, lat, lon);
+      console.log("SPC-Outlook: Day 2 Hail Risk" + day2HailRisk)
+      //wind
+      url = "https://www.spc.noaa.gov/products/outlook/day2otlk_wind.lyr.geojson";
+      response = await fetch(url);
+      geojson = await response.json();
+      const day2WindRisk = this.checkDayPerc(geojson, lat, lon);
+      console.log("SPC-Outlook: Day 2 Wind Risk" + day1WindRisk)
+
+      if (day2TorRisk > 0 || day2HailRisk > 0 || day2WindRisk > 0) day2ProbRisk = true;
+      console.log("SPC-Outlook: Day 2 Prob Risk test | " + day2ProbRisk)
+
+      url = "https://www.spc.noaa.gov/products/outlook/day3otlk_cat.lyr.geojson";
+      response = await fetch(url);
+      geojson = await response.json();
+      const day3Risk = this.checkDayCat(geojson, lat, lon, riskToValue, valueToRisk);
+      //console.log("SPC-Outlook: Day 2 Risk got - " + this.day2Risk);
+      url = "https://www.spc.noaa.gov/products/outlook/day3otlk_prob.lyr.geojson";
+      response = await fetch(url);
+      geojson = await response.json();
+      const day3ProbRisk = this.checkDayPerc(geojson, lat, lon);
+      console.log("SPC-Outlook: Day 2 Prob Risk" + day3ProbRisk)
 
       return {
-        day1: {"risk": day1Risk, "text": valueToFullRisk[day1Risk], "color": riskToColor[day1Risk]}
+        day1: {
+          "risk": day1Risk,
+          "text": valueToFullRisk[day1Risk],
+          "color": riskToColor[day1Risk],
+          "probRisk": day1ProbRisk,
+          "torRisk": day1TorRisk,
+          "torSign": day1TorSign,
+          "hailRisk": day1HailRisk,
+          "hailSign": day1HailSign,
+          "windRisk": day1WindRisk,
+          "windSign": day1WindSign
+        },
+        day2: {"risk": day2Risk, "text": valueToFullRisk[day2Risk], "color": riskToColor[day2Risk], "probRisk": day2ProbRisk, "torRisk": day2TorRisk, "hailRisk": day2HailRisk, "windRisk": day2WindRisk},
+        day3: {"risk": day3Risk, "text": valueToFullRisk[day3Risk], "color": riskToColor[day3Risk], "probRisk": day3ProbRisk}
       };
 
     } catch (err) {
@@ -86,7 +163,54 @@ module.exports = NodeHelper.create({
         }
       }
     }
-
+    //console.log("SPC outlook Debug: result = " + highestValue + "|" + valueToRisk[highestValue])
     return highestValue === 0 ? "NONE" : valueToRisk[highestValue];
+  },
+
+  checkDayPerc(geojson, lat, lon) {
+    let highestValue = 0;
+    const pt = turf.point([lon, lat]);
+
+    for (const feature of geojson.features) {
+      if (!feature.geometry) continue;
+
+      // For polygons vs multipolygons:
+      const geomType = feature.geometry.type;
+      const labelValue = feature.properties.LABEL;
+
+      if (geomType === "Polygon") {
+        const poly = turf.polygon(feature.geometry.coordinates);
+        if (turf.booleanPointInPolygon(pt, poly) && labelValue > highestValue && labelValue != "SIGN") {
+          highestValue = labelValue;
+        }
+      } else if (geomType === "MultiPolygon") {
+        const multiPoly = turf.multiPolygon(feature.geometry.coordinates);
+        if (turf.booleanPointInPolygon(pt, multiPoly) && labelValue > highestValue && labelValue != "SIGN") {
+          highestValue = labelValue;
+        }
+      }
+    }
+    console.log("SPC outlook Debug: result = " + highestValue)
+    return highestValue
+  },
+
+  checkDaySign(geojson, lat, lon) {
+  const pt = turf.point([lon, lat]);
+  for (const feature of geojson.features) {
+    if (!feature.geometry) continue;
+    // Only process features that are flagged as SIG
+    if (feature.properties.LABEL === "SIGN") {
+      let polygon;
+      if (feature.geometry.type === "Polygon") {
+        polygon = turf.polygon(feature.geometry.coordinates);
+      } else if (feature.geometry.type === "MultiPolygon") {
+        polygon = turf.multiPolygon(feature.geometry.coordinates);
+      }
+      if (turf.booleanPointInPolygon(pt, polygon)) {
+        return true;
+      }
+    }
   }
+  return false;
+}
 });
